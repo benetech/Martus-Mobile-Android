dfb022a1cb186908c329c4cf0277b39560e9539d
Rom Srinivasan <Rom Srinivasan@localhost>
2013-10-17 10:18:31 -0700

    indicating which ODK files were modified extensively

--- martus-android/src/org/odk/collect/android/activities/FormHierarchyActivity.java
+++ martus-android/src/org/odk/collect/android/activities/FormHierarchyActivity.java
@@ -10,6 +10,9 @@
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
+ *
+ *  This class modified extensively by Benetech (Rom Srinivasan)
+ *
  */
 
 package org.odk.collect.android.activities;
