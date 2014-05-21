66c729b48685cbf2a80ae088a8b58c09cf354f16
Nima A. <nimaa@benetech.org>
2014-05-21 10:54:37 -0400

    Refactor import organize

--- martus-android/src/org/martus/android/dialog/LoginDialog.java
+++ martus-android/src/org/martus/android/dialog/LoginDialog.java
@@ -2,8 +2,6 @@ package org.martus.android.dialog;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.view.KeyEvent;
