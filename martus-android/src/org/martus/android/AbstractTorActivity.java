8ae007db8deef759759d7d4e8fce08ddac19aa2b
Nima A. <nimaa@benetech.org>
2014-06-02 17:44:30 -0400

    Refactor organize imports

--- martus-android/src/org/martus/android/AbstractTorActivity.java
+++ martus-android/src/org/martus/android/AbstractTorActivity.java
@@ -4,8 +4,6 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.CompoundButton;
 
-import java.util.Properties;
-
 import info.guardianproject.onionkit.ui.OrbotHelper;
 
 /**
