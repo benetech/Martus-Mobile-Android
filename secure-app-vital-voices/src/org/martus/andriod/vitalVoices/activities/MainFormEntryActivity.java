9d4f0d6345efd7189ed1b5476d208dd98db0577d
Nima A. <nimaa@benetech.org>
2014-08-08 10:38:38 -0400

    Martus Mobile Android Library shell.  This will contain common android code that is shared between apps

---
+++ secure-app-vital-voices/src/org/martus/andriod/vitalVoices/activities/MainFormEntryActivity.java
@@ -0,0 +1,11 @@
+package org.martus.andriod.vitalVoices.activities;
+
+import android.app.Activity;
+
+import org.odk.collect.android.activities.FormEntryActivity;
+
+/**
+ * Created by nimaa on 7/25/14.
+ */
+public class MainFormEntryActivity extends FormEntryActivity {
+}
