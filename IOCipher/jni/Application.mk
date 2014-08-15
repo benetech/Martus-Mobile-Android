62300a05cfd035b0382a8cf93c7652fa02c828d5
David Brodsky <dbro@dbro.pro>
2014-08-15 15:56:36 -0400

    Replace IOCipher repository with binary release .jars
    IOCipher is a git subrepository with submodules,
    the copy & pasted repository did not include these. Short of
    adding IOCipher as a subrepository, sticking to the binary release
    seems best.

--- IOCipher/jni/Application.mk
+++
@@ -1,6 +0,0 @@
-NDK_TOOLCHAIN_VERSION=4.4.3
-APP_PROJECT_PATH := $(shell pwd)
-APP_BUILD_SCRIPT := $(APP_PROJECT_PATH)/jni/Android.mk
-APP_STL := stlport_static
-#
-EXTERNAL_PATH := $(APP_PROJECT_PATH)/external
